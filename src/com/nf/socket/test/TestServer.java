828c2a4fa10939245f1a7c8dc52de903eeb98627
yaonengchao <nongfu.n.yao@qq.com>
2021-02-08 11:47:47 +0800

    测试用例

--- src/com/nf/socket/test/TestServer.java
+++ src/com/nf/socket/test/TestServer.java
@@ -1,35 +1,38 @@
 package com.nf.socket.test;
 
 import com.nf.socket.constant.Constant;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
 
 public class TestServer {
 
-    public static void main(String[] args) {
 
+    @Test
+    public void tcpClientMain() {
+
+        CountDownLatch countDownLatch = new CountDownLatch(5);
         for (int j = 0; j < 5; j++) {
 
-            new Thread(()->{
+            new Thread(() -> {
 
                 System.out.println("客户端启动。。。。。");
                 Socket socket = null;
                 try {
                     socket = new Socket(Constant.IP, Constant.PORT);
-                    System.out.println(socket);
                     String sendMsg = null;
-                    BufferedReader bufferedReader = null;
-                    PrintWriter printWriter = null;
                     if (socket.isConnected()) {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
                         try {
-                            printWriter = new PrintWriter(socket.getOutputStream());
-                            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                             for (int i = 0; i < 100; i++) {
-
                                 switch (i % 4) {
                                     case 0:
                                         sendMsg = "GET:dog";
@@ -47,7 +50,6 @@ public class TestServer {
                                     default:
                                         break;
                                 }
-
                                 printWriter.println(sendMsg);
                                 printWriter.flush();
                                 //确认消息
@@ -59,10 +61,8 @@ public class TestServer {
                             }
 
                         } finally {
-                            if (printWriter != null)
-                                printWriter.close();
-                            if (bufferedReader != null)
-                                bufferedReader.close();
+                            printWriter.close();
+                            bufferedReader.close();
                         }
                     }
                     if (!socket.isClosed()) {
@@ -72,16 +72,61 @@ public class TestServer {
                             e.printStackTrace();
                         }
                     }
-
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
+                countDownLatch.countDown();
             }).start();
+        }
 
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
+
+    @AfterEach
+    public void after() {
+        Socket socket = null;
+        try {
+            socket = new Socket(Constant.IP, Constant.PORT);
+            String sendMsg = null;
+            if (socket.isConnected()) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                try {
+                    printWriter.println("LIST");
+                    printWriter.flush();
+                    //确认消息
+                    String receiveMsg = null;//客户端发过来的信息
+                    char[] ch = new char[1024];
+                    int read = bufferedReader.read(ch);
+                    receiveMsg = new String(ch, 0, read);
+
+
+                    Assertions.assertEquals("cat:125\ndog:125\nchicken:125\nparrot:125\nOK",receiveMsg, "数据结果错误");
+                    System.out.print(receiveMsg);
+
+                } finally {
+                    printWriter.close();
+                    bufferedReader.close();
+                }
+            }
+            if (!socket.isClosed()) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
